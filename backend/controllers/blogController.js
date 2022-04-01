a041e6967529deba76a3158d0e1253db70a244af
oguzhan <oguzhantekeli@gmail.com>
2022-04-01 15:24:17 +0300

    update blog debugged

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -26,14 +26,17 @@ const getBlogs = asyncHandler(async (req, res) => {
 // access private/(public before auth)
 const getBlog = asyncHandler(async (req, res) => {
   const blog = await Blog.findById(req.params.id);
-  // const comments = await Comment.find({ blogId: req.params.id });
+  if (!blog) {
+    res.status(400);
+    throw new Error("Blog Not Found");
+  }
   res.status(200).json({
     id: blog._id,
     title: blog.title,
     text: blog.text,
     author: blog.author,
-    email: blog.email,
-    registered: blog.registered,
+    status: blog.status,
+    updatedAt: blog.updatedAt,
     imageBigUrl: blog.imageBigUrl,
     imageThumbUrl: blog.imageThumbUrl,
     category: blog.category,
@@ -72,7 +75,6 @@ const setBlog = asyncHandler(async (req, res) => {
     title: blog.title,
     text: blog.text,
     author: blog.author,
-    email: blog.email,
     updatedAt: blog.updatedAt,
     imageBigUrl: blog.imageBigUrl,
     imageThumbUrl: blog.imageThumbUrl,
@@ -87,24 +89,21 @@ const setBlog = asyncHandler(async (req, res) => {
 // access private/(public before auth)
 const updateBlog = asyncHandler(async (req, res) => {
   const blog = await Blog.findById(req.params.id);
+  if (blog.id.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Unauthenticated action");
+  }
+
   if (!blog) {
     res.status(400);
     throw new Error("blog not found");
   }
-
-  if (!req.user) {
-    res.status(401);
-    throw new Error("Unauthenticated action");
+  if (!req.body.title || !req.body.text || !req.body.category) {
+    res.status(400);
+    throw new Error("Required fiedls must be set");
   }
 
-  if (blog.user.toString() !== req.user.id) {
-    res.status(401);
-    throw new Error("Unauthenticated action");
-  }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body);
   res.status(200).json({
     id: updatedBlog._id,
     title: updatedBlog.title,
